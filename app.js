1d8c78bfc5c0abd127f5484af3c000c60acf30f3
AraujjoDev <victoxx@gmail.com>
2025-03-11 21:18:10 -0300

    commit inicial

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+let sorteados = [];
+
+function adicionarAmigo(){
+    let entrada = document.querySelector("input").value;
+    
+
+    if (entrada == 0 ){
+        console.log("Por favor, insira um nome.");
+    } else if (entrada < 0 || entrada > 0 ) {
+        console.log("Digite um nome")
+    }
+    else {
+        amigos.push(entrada);
+    }
+
+}  
+
+function limparCampo(campo){
+    if (adicionarAmigo(campo.value))
+    campo.value = "";
+}
